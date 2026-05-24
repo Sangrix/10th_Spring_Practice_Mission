5854e99d37edef73c3f6b34069095e92f60dd2de
ChoiMinSang <min393917@gmail.com>
2026-05-24 17:20:58 +0900

    fix: dependency error resolve

--- src/test/java/umc10th/Umc10thApplicationTests.java
+++ src/test/java/umc10th/Umc10thApplicationTests.java
@@ -1,10 +1,10 @@
-package umc;
+package umc10th;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class Umc10thApplicationTests {
 
 	@Test
 	void contextLoads() {
